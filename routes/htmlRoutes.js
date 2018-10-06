b6ee7d09739dcf65e595671cc83b14c472e743de
Muddy Lee <muddy@Muddys-iMac.local>
2018-10-05 19:07:26 -0700

    put html into handlebar format, made .js file for ajax calls, image manipulation, and folder to hold images

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,7 +13,7 @@ module.exports = function(app) {
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Story.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
