efdbc7ec5e17a03c9ab6a4e5b3f4d41ee48fb823
vflores <valerie.flores64@gmail.com>
2018-10-07 21:23:27 -0700

    updated formatting to match eslint. Again.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,24 +3,25 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Story.findAll({}).then(function() {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: [{ id: 1, text: "Hello" }, { id: 2, text: " World" }]
-      });
+    db.Story.findAll({}).then(function(dbStory) {
+      var hbsObject = {
+        stories: dbStory
+      };
+      res.render("index", hbsObject);
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Story.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Load New Story modal
+  app.get("/modal", function(req, res) {
+    res.render("modal");
   });
+
+  // Load example page and pass in an example by id
+  // app.get("/stories/:storyId?", function(req, res) {
+  //   db.Story.findOne({}).then(function(dbStory) {
+  //     res.json(dbStory);
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
