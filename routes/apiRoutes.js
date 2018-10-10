08033e493565c79844dd4d4a36db5952addc9836
vflores <valerie.flores64@gmail.com>
2018-10-09 21:55:02 -0700

    updated card block in index.handlebars, removed unordered list

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,33 +1,10 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get most recent (default setting)
-  app.get("/", function(req, res) {
-    var query = {};
-    if (req.query.user_id) {
-      query.UserId = req.query.user_id;
-    }
-    db.Entries.findAll({
-      include: [db.User],
-      where: query
-    }).then(function(data) {
-      var hbsObject = {
-        entries: data
-      };
-      res.render("index", hbsObject);
-      // console.log(hbsObject);
-    });
-  });
-
-  // // Get most popular
-
-  // // Find a specific author
-
   // Create a new story
   app.post("/api/entries", function(req, res) {
     db.Entries.create({
       title: req.body.title,
-      author: req.body.author,
       entry: req.body.entry
     }).then(function(dbStory) {
       res.json({ id: dbStory.insertId });
