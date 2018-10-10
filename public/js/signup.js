76c4becab47161ffbbfd0b74c05d031711a9d9ea
Hopp Willi <38337505+jhwillia7@users.noreply.github.com>
2018-10-10 11:44:12 -0700

    v5

--- public/js/signup.js
+++ public/js/signup.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
       password: password
     })
       .then(function(data) {
-     //   window.location.replace(data);
+        window.location.replace(data);
         // If there's an error, handle it by throwing up a bootstrap alert
         console.log(data);
       })
