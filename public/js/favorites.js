500db6364e2d160db752e45592a1669caf5d0bcc
Ray Ventura <circfox@yahoo.com>
2018-10-13 14:27:41 -0700

    this implemented the heart and a fake api route

--- public/js/favorites.js
+++ public/js/favorites.js
@@ -1,7 +1,17 @@
-$(document).ready(function(){
-  $("#far fa-heart").on("click", solid);
+$(document).ready(function() {
+  $(".far.fa-heart").on("click", solid);
 });
 
 function solid() {
-  $(this).addClass("fas")
+  var id = this.id;
+  var heart = this;
+  $.post("/api/fav/" + id)
+    .then(function() {
+      $(heart)
+        .addClass("fas")
+        .removeClass("far");
+    })
+    .catch(function() {
+      console.log("unable to favorite");
+    });
 }
